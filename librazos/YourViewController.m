5d7b79cdfe4f342018da19231c30b3f3138f417a
cristianprofile <cristianprofile@gmail.com>
2014-05-28 01:06:09 +0200

    added google maps view

--- librazos/YourViewController.m
+++ librazos/YourViewController.m
@@ -1,8 +1,16 @@
 #import "YourViewController.h"
 #import <GoogleMaps/GoogleMaps.h>
 
+
+
+static NSString const * kNormalType = @"Normal";
+static NSString const * kSatelliteType = @"Satellite";
+static NSString const * kHybridType = @"Hybrid";
+static NSString const * kTerrainType = @"Terrain";
+
 @implementation YourViewController {
     GMSMapView *mapView_;
+    UISegmentedControl *switcher_;
 }
 
 - (void)viewDidLoad {
@@ -12,16 +20,37 @@
                                                             longitude:-3.7497285227539123
                                                                  zoom:16];
     mapView_ = [GMSMapView mapWithFrame:CGRectZero camera:camera];
-    mapView_.myLocationEnabled = YES;
     self.view = mapView_;
     
-    // Creates a marker in the center of the map.
+    // The possible different types to show.
+    NSArray *types = @[kNormalType, kSatelliteType, kHybridType, kTerrainType];
+    
+    // Create a UISegmentedControl that is the navigationItem's titleView.
+    switcher_ = [[UISegmentedControl alloc] initWithItems:types];
+    switcher_.autoresizingMask = UIViewAutoresizingFlexibleBottomMargin |
+    UIViewAutoresizingFlexibleWidth |
+    UIViewAutoresizingFlexibleBottomMargin;
+    switcher_.selectedSegmentIndex = 0;
+    self.navigationItem.titleView = switcher_;
+    
+    // Listen to touch events on the UISegmentedControl.
+    [switcher_ addTarget:self action:@selector(didChangeSwitcher)
+        forControlEvents:UIControlEventValueChanged];
+}
 
-    GMSMarker *marker = [[GMSMarker alloc] init];
-    marker.position = CLLocationCoordinate2DMake(40.33468913769062, -3.7497285227539123);
-    marker.title = @"Sydney";
-    marker.snippet = @"Australia";
-    marker.map = mapView_;
+- (void)didChangeSwitcher {
+    // Switch to the type clicked on.
+    NSString *title =
+    [switcher_ titleForSegmentAtIndex:switcher_.selectedSegmentIndex];
+    if ([kNormalType isEqualToString:title]) {
+        mapView_.mapType = kGMSTypeNormal;
+    } else if ([kSatelliteType isEqualToString:title]) {
+        mapView_.mapType = kGMSTypeSatellite;
+    } else if ([kHybridType isEqualToString:title]) {
+        mapView_.mapType = kGMSTypeHybrid;
+    } else if ([kTerrainType isEqualToString:title]) {
+        mapView_.mapType = kGMSTypeTerrain;
+    }
 }
 
 @end
